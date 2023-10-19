0695f1ec624c12ce3970cbc004674deea7b2992e
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2023-10-19 14:35:06 -0700

    lg - added service to get information (category and number) for jokes

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
@@ -1,11 +1,12 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-
+@Slf4j
 @Service
 public class JokeQueryService {
 
