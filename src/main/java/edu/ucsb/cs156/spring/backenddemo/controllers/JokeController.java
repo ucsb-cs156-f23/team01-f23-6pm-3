96c53b5d5efe7d50999ecc3e907f2046fbf70e9d
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2023-10-20 15:39:06 -0700

    Add JokeController

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/JokeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/JokeController.java
@@ -2,7 +2,33 @@ package edu.ucsb.cs156.spring.backenddemo.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.cs156.spring.backenddemo.services.JokeQueryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Tag(name="Joke info from https://v2.jokeapi.dev/")
 @RestController
+@RequestMapping("/api/jokes")
 public class JokeController {
     
+    ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    JokeQueryService jokeQueryService;
+
+    @Operation(summary = "Get a random joke")
+    @GetMapping("/get")
+    public ResponseEntity<String> getJoke() throws JsonProcessingException {
+        String result = jokeQueryService.getJSON("Programming", 1);  // Placeholder for category and number of jokes. Actual implementation might differ.
+        return ResponseEntity.ok().body(result);
+    }
+
 }
